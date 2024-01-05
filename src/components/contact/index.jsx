d1af55ce4c9333efb51b38eb71c581a50a08071d
GonzaloRobledo <gonza_roble99@hotmail.com>
2024-01-04 23:12:57 -0300

    contact_hero

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -7,6 +7,7 @@ import ContactInner from "./contact-inner";
 import CtaArea from "./cta-area";
 import HeroBanner from "../../common/hero-banner";
 import OfficeLocation from "./office-location";
+import BreadcrumbTwo from "@/src/common/breadcrumbs/breadcrumb-2";
 
 const Contact = () => {
   return (
@@ -15,10 +16,11 @@ const Contact = () => {
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <main>
-            <Breadcrumb title_top="Get In" title_bottom="Get In" />
-            <HeroBanner bg_img="/assets/img/contact/contact-banner.jpg" />
-            <OfficeLocation />
-            <ContactFormArea />
+            {/* <Breadcrumb title_top="Get In" title_bottom="Get In" /> */}
+            {/* <HeroBanner bg_img="/assets/img/contact/contact-banner.jpg" /> */}
+            {/* <OfficeLocation /> */}
+            <BreadcrumbTwo title={'Contact Us'} innertitle={'Contact'} />
+            <div style={{marginTop:120}}><ContactFormArea /></div>
             <ContactInner />
             <CtaArea />
           </main>
