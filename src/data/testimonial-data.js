93d7617e153fc229bd624bdc4dbcdbb8b109b40e
miradario <miradario@gmail.com>
2024-01-19 17:49:47 -0300

    testimonials

--- src/data/testimonial-data.js
+++ src/data/testimonial-data.js
@@ -1,6 +1,7 @@
 import author_img_2 from '../../public/assets/img/testimonial/testi-icon-1.jpeg'
-import author_img_3 from '../../public/assets/img/testimonial/testi-icon-5-4.png'
-import author_img_4 from '../../public/assets/img/testimonial/testi-icon-5-3.png'
+import eugene from '../../public/assets/img/testimonial/eugene.jpeg'
+import veronica from '../../public/assets/img/testimonial/veronica.jpeg'
+import andres from '../../public/assets/img/testimonial/andres.jpeg'
 import author_img_5 from '../../public/assets/img/testimonial/testi-icon-5-4.png'
 import author_img_6 from '../../public/assets/img/testimonial/testi-icon-5-1.png'
 import author_img_8 from '../../public/assets/img/testimonial/testi-icon-5-2.png'
@@ -8,9 +9,9 @@ import author_img_9 from '../../public/assets/img/testimonial/testi-icon-5-4.png
 import author_img_12 from '../../public/assets/img/testimonial/santi.jpeg'
 
 import brand_icon_2 from '../../public/assets/img/testimonial/atlantic0.png'
-import brand_icon_3 from '../../public/assets/img/testimonial/testi-logo-5-2.png'
-import brand_icon_4 from '../../public/assets/img/testimonial/testi-logo-5-3.png'
-import brand_icon_5 from '../../public/assets/img/testimonial/testi-logo-5-3.png'
+import islandpay from '../../public/assets/img/testimonial/islandpay.svg'
+import infinite from '../../public/assets/img/testimonial/infinitepower.png'
+import gds from '../../public/assets/img/testimonial/gdsc.png'
 import brand_icon_6 from '../../public/assets/img/testimonial/testi-logo-5-2.png'
 import brand_icon_8 from '../../public/assets/img/testimonial/testi-logo-5-1.png'
 import brand_icon_9 from '../../public/assets/img/testimonial/testi-logo-5-2.png'
@@ -21,7 +22,7 @@ const testimonial_data = [
     id: 12,
     author_img: author_img_12,
     brand_icon: brand_icon_12,
-    name: 'Santiago Facal (1)',
+    name: 'Santiago Facal',
     title: ' Product Manager',
     description: (
       <>
@@ -39,7 +40,7 @@ const testimonial_data = [
     id: 13,
     author_img: author_img_2,
     brand_icon: brand_icon_2,
-    name: 'Helen Stewart (2)',
+    name: 'Helen Stewart',
     title: 'Director',
     description: (
       <>
@@ -53,116 +54,73 @@ const testimonial_data = [
   },
   {
     id: 14,
-    author_img: author_img_2,
-    brand_icon: brand_icon_2,
-    name: '2',
-    title: 'Community Organiser',
+    author_img: eugene,
+    brand_icon: islandpay,
+    name: 'Eugene Luzgin',
+    title: 'CTO ',
     description: (
       <>
-        “ This is really awesome. many small businessmen are suffering from this
-        service in different platform and the lovers are always looking this
-        kind of feature on their own website. ”
+        “ Highly recommend Gurudevelopers - they has great skills in IOS development area. Will gladly work again when such need arises.”
       </>
     )
   },
   {
     id: 15,
-    author_img: author_img_3,
-    brand_icon: brand_icon_3,
-    name: '3',
-    title: 'Community Organiser',
+    author_img: author_img_12,
+    brand_icon: brand_icon_12,
+    name: 'Santiago Facal',
+    title: ' Product Manager',
     description: (
       <>
-        “ This is really awesome. many small businessmen are suffering from this
-        service in different platform and the lovers are always looking this
-        kind of feature on their own website. ”
+        “ The results are even more than expected, he and his team are
+        responsible and fully committed to every project we work on. They
+        responds to changing and sometimes challenging situations, always
+        maintaining his positive attitude and working hard to meet all agreed
+        deadlines. Always proactive, adapts easily to changes and has a great
+        capacity for teamwork. If I had the chance to choose, I will always
+        choose him and his team to work on all projects. ”
       </>
     )
   },
   {
     id: 16,
-    author_img: author_img_4,
-    brand_icon: brand_icon_4,
-    name: '4',
-    title: 'Community Organiser',
+    author_img: author_img_2,
+    brand_icon: brand_icon_2,
+    name: 'Helen Stewart',
+    title: 'Director',
     description: (
       <>
-        “ This is really awesome. many small businessmen are suffering from this
-        service in different platform and the lovers are always looking this
-        kind of feature on their own website. ”
+        We have had a wonderful experience working with Dario and his team. They
+        communicate well and are eager and highly pleasant to work with. The
+        work is thorough and they are attentive to details and the particulars
+        of the requirements. We look forward to working with them again on
+        future projects. Cannot recommend them enough.
       </>
     )
   },
   {
     id: 17,
-    author_img: author_img_5,
-    brand_icon: brand_icon_5,
-    name: '5',
-    title: 'Community Organiser',
+    author_img: andres,
+    brand_icon: gds,
+    name: 'Andres Hernandez',
+    title: 'CEO ',
     description: (
       <>
-        “ This is really awesome. many small businessmen are suffering from this
-        service in different platform and the lovers are always looking this
-        kind of feature on their own website. ”
+        “As the Director of GDS, I have had the privilege of working on several projects . Their performance has always been extraordinary, not only in technical aspects but also in his interpersonal skills with peers, superiors, and subordinates. Additionally, their ability to lead and motivate stood out. They are reliable, honest, proactive, and possesses special abilities to thrive successfully in multidisciplinary work teams.”
       </>
     )
   },
   {
     id: 18,
-    author_img: author_img_6,
-    brand_icon: brand_icon_6,
-    name: '6',
-    title: 'Community Organiser',
+    author_img: veronica,
+    brand_icon: infinite,
+    name: 'Veronica Quiñonez',
+    title: 'Office Manager',
     description: (
       <>
-        “ This is really awesome. many small businessmen are suffering from this
-        service in different platform and the lovers are always looking this
-        kind of feature on their own website. ”
-      </>
-    )
-  },
-
-  {
-    id: 20,
-    author_img: author_img_8,
-    brand_icon: brand_icon_8,
-    name: '7',
-    title: 'Community Organiser',
-    description: (
-      <>
-        “ This is really awesome. many small businessmen are suffering from this
-        service in different platform and the lovers are always looking this
-        kind of feature on their own website. ”
-      </>
-    )
-  },
-  {
-    id: 21,
-    author_img: author_img_9,
-    brand_icon: brand_icon_9,
-    name: '8',
-    title: 'Community Organiser',
-    description: (
-      <>
-        “ This is really awesome. many small businessmen are suffering from this
-        service in different platform and the lovers are always looking this
-        kind of feature on their own website. ”
+        “A pleasure to work with Agustín and Darío, good communication and on time delivery. Thanks guys!”
       </>
     )
   }
-  //   {
-  //     id: 22,
-  //     author_img: author_img_10,
-  //     brand_icon: brand_icon_10,
-  //     name: 'Gary M. Adams',
-  //     title: 'Community Organiser',
-  //     description: (
-  //       <>
-  //         “ This is really awesome. many small businessmen are suffering from this
-  //         service in different platform and the lovers are always looking this
-  //         kind of feature on their own website. ”
-  //       </>
-  //     )
-  //   }
 ]
 export default testimonial_data
