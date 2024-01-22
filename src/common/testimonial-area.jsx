4f19ea394ee0ef6bff97385b4bcd69373e1b7230
GonzaloRobledo <gonza_roble99@hotmail.com>
2024-01-22 17:05:22 -0300

    carousel_testimonials

--- src/common/testimonial-area.jsx
+++ src/common/testimonial-area.jsx
@@ -1,129 +1,77 @@
-'use client'
-import testimonial_data from '@/src/data/testimonial-data';
-import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Scrollbar } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa6'
+import testimonial_data from '../data/testimonial-data'
+import Image from 'next/image'
 
-const testimonial_content = {
-   bg_img: "/assets/img/testimonial/testi-bg-5-1.jpg",
-   sub_title: "CLIENTS & PROJECTS",
-   title: <>About Customer <span>Stories</span></>,
-}
-const { bg_img, sub_title, title } = testimonial_content
+const testimonials = testimonial_data
 
-// setting 
 const setting = {
-   loop: true,
-   slidesPerView: 4,
-   spaceBetween: 30,
-   breakpoints: {
-      '1200': {
-         slidesPerView: 4,
-      },
-      '992': {
-         slidesPerView: 3,
-      },
-      '768': {
-         slidesPerView: 3,
-      },
-      '576': {
-         slidesPerView: 1,
-      },
-      '0': {
-         slidesPerView: 1,
-      },
-   },
-   scrollbar: {
-      el: ".tp-scrollbar",
-      clickable: true,
-   },
-   navigation: {
-      nextEl: '.test-prev',
-      prevEl: '.test-next',
-   },
+  loop: true,
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  // breakpoints: {
+  //    '1200': {
+  //       slidesPerView: 4,
+  //    },
+  //    '992': {
+  //       slidesPerView: 3,
+  //    },
+  //    '768': {
+  //       slidesPerView: 3,
+  //    },
+  //    '576': {
+  //       slidesPerView: 1,
+  //    },
+  //    '0': {
+  //       slidesPerView: 1,
+  //    },
+  // },
+  // scrollbar: {
+  //    el: ".tp-scrollbar",
+  //    clickable: true,
+  // },
+  navigation: {
+    nextEl: '.next_arrow',
+    prevEl: '.prev_arrow'
+  }
 }
 
-const TestimonialArea = () => {
-
-   const [isDragged, setIsDragged] = useState(false);
-
-   const handleSlideChange = () => {
-      setIsDragged(true);
-   };
-
-   const handleTransitionEnd = () => {
-      setIsDragged(false);
-   };
-
-   return (
-      <>
-         <div className="tp-testimonial-area pt-130 pb-130 fix"
-            style={{ marginBottom:100, position:'relative' }}
-         >
-            <img src={bg_img} alt="theme-pure" style={{objectFit:'cover', width:'100%', height:'100%', position:'absolute', top:0,left:0}}/>
-            <div className="container" style={{position:'relative',zIndex:100}}>
-               <div className="row align-items-end tp-testimonial-five-section-space">
-                  <div className="col-md-8">
-                     <div className="tp-testimonial-five-section-box">
-                        <span className="tp-section-subtitle-5">{sub_title}</span>
-                        <h3 className="tp-section-title-5">{title}</h3>
-                     </div>
-                  </div>
-                  <div className="col-md-4">
-                     <div className="tp-testimonial-five-arrow d-flex align-items-center justify-content-start justify-content-md-end">
-                        <div className="test-next">
-                           <button><i className="far fa-arrow-left"></i></button>
-                        </div>
-                        <div className="test-prev">
-                           <button><i className="far fa-arrow-right"></i></button>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div className="container-fluid p-0">
-               <div className="row g-0">
-                  <div className="col-12">
-                     <div className="tp-testimonial-five-slider-section">
-                        <Swiper
-                           {...setting}
-                           onSliderMove={handleSlideChange}
-                           onTransitionEnd={handleTransitionEnd}
-                           modules={[Navigation, Scrollbar]}
-                           className={`swiper-container testimonial-five-slider-active ${isDragged ? "dragged" : ""
-                              }`}>
-                           {testimonial_data.map((item, i) =>
-                              <SwiperSlide key={i} className="tp-testimonial-five-item">
-                                 <div className="tp-testimonial-five-wrapper d-flex justify-content-between align-items-center">
-                                    <div className="tp-testimonial-five-top-info d-flex align-items-center">
-                                       <div className="tp-testimonial-five-avata">
-                                          <Image src={item.author_img} alt="theme-pure" />
-                                       </div>
-                                       <div className="tp-testimonial-five-author-info">
-                                          <h4>{item.name}</h4>
-                                          <span>{item.title}</span>
-                                       </div>
-                                    </div>
-                                    <div className="tp-testimonial-five-brand d-none d-sm-block">
-                                       <Image src={item.brand_icon} alt="theme-pure" />
-                                    </div>
-                                 </div>
-                                 <div className="tp-testimonial-five-content">
-                                    <p>{item.description}</p>
-                                 </div>
-                              </SwiperSlide>
-                           )
-                           }
-                        </Swiper>
-                        <div className="tp-scrollbar"></div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </>
-   );
-};
-
-export default TestimonialArea;
+export default function TestimonialArea () {
+  return (
+    <article className='container container_testimonials'>
+      <div className='prev_arrow prev_arrow_desktop'><FaChevronLeft  /></div>
+      <div className='container_swiper'>
+        <Swiper {...setting} modules={[Navigation]}>
+          {testimonials.map((el, index) => (
+            <SwiperSlide key={el.name + '-' + index} className='testimonial'>
+              <div>
+                <div style={{display:'flex', justifyContent:'center'}}>
+                    <div className="image_testimonial">
+                        <Image
+                          src={el.author_img}
+                          alt='theme-pure'
+                          width={100}
+                          height={100}
+                        />
+                    </div>
+                </div>
+                <p className="description">{el.description}</p>
+                <div className="author">
+                  <h3>{el.name}</h3>
+                  <span>{el.title}</span>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="arrows_testimonials_responsive">
+            <div className='prev_arrow'><FaChevronLeft  /></div>
+            <div className='next_arrow'><FaChevronRight  /></div>
+        </div>
+      </div>
+      <div className='next_arrow next_arrow_desktop'><FaChevronRight  /></div>
+    </article>
+  )
+}
--- src/data/testimonial-data.js
+++ src/data/testimonial-data.js
@@ -24,17 +24,8 @@ const testimonial_data = [
     brand_icon: brand_icon_12,
     name: 'Santiago Facal',
     title: ' Product Manager',
-    description: (
-      <>
-        “ The results are even more than expected, he and his team are
-        responsible and fully committed to every project we work on. They
-        responds to changing and sometimes challenging situations, always
-        maintaining his positive attitude and working hard to meet all agreed
-        deadlines. Always proactive, adapts easily to changes and has a great
-        capacity for teamwork. If I had the chance to choose, I will always
-        choose him and his team to work on all projects. ”
-      </>
-    )
+    description:
+      '“ The results are even more than expected, he and his team are responsible and fully committed to every project we work on. They responds to changing and sometimes challenging situations, always maintaining his positive attitude and working hard to meet all agreed deadlines. Always proactive, adapts easily to changes and has a great capacity for teamwork. If I had the chance to choose, I will always choose him and his team to work on all projects. ”'
   },
   {
     id: 13,
@@ -42,15 +33,8 @@ const testimonial_data = [
     brand_icon: brand_icon_2,
     name: 'Helen Stewart',
     title: 'Director',
-    description: (
-      <>
-        We have had a wonderful experience working with Dario and his team. They
-        communicate well and are eager and highly pleasant to work with. The
-        work is thorough and they are attentive to details and the particulars
-        of the requirements. We look forward to working with them again on
-        future projects. Cannot recommend them enough.
-      </>
-    )
+    description:
+      'We have had a wonderful experience working with Dario and his team. They communicate well and are eager and highly pleasant to work with. The work is thorough and they are attentive to details and the particulars of the requirements. We look forward to working with them again on future projects. Cannot recommend them enough.'
   },
   {
     id: 14,
@@ -58,12 +42,8 @@ const testimonial_data = [
     brand_icon: islandpay,
     name: 'Eugene Luzgin',
     title: 'CTO ',
-    description: (
-      <>
-        “ Highly recommend Gurudevelopers - they has great skills in IOS
-        development area. Will gladly work again when such need arises.”
-      </>
-    )
+    description:
+      '“ Highly recommend Gurudevelopers - they has great skills in IOS development area. Will gladly work again when such need arises.”'
   },
   {
     id: 17,
@@ -71,17 +51,8 @@ const testimonial_data = [
     brand_icon: gds,
     name: 'Andres Hernandez',
     title: 'CEO ',
-    description: (
-      <>
-        “As the Director of GDS, I have had the privilege of working on several
-        projects . Their performance has always been extraordinary, not only in
-        technical aspects but also in his interpersonal skills with peers,
-        superiors, and subordinates. Additionally, their ability to lead and
-        motivate stood out. They are reliable, honest, proactive, and possesses
-        special abilities to thrive successfully in multidisciplinary work
-        teams.”
-      </>
-    )
+    description:
+      '“As the Director of GDS, I have had the privilege of working on several projects . Their performance has always been extraordinary, not only in technical aspects but also in his interpersonal skills with peers, superiors, and subordinates. Additionally, their ability to lead and motivate stood out. They are reliable, honest, proactive, and possesses special abilities to thrive successfully in multidisciplinary work teams.”'
   },
   {
     id: 18,
@@ -89,90 +60,8 @@ const testimonial_data = [
     brand_icon: infinite,
     name: 'Veronica Quiñonez',
     title: 'Office Manager',
-    description: (
-      <>
-        “A pleasure to work with Agustín and Darío, good communication and on
-        time delivery. Thanks guys!”
-      </>
-    )
-  },
-  {
-    id: 19,
-    author_img: author_img_12,
-    brand_icon: brand_icon_12,
-    name: 'Santiago Facal',
-    title: ' Product Manager',
-    description: (
-      <>
-        “ The results are even more than expected, he and his team are
-        responsible and fully committed to every project we work on. They
-        responds to changing and sometimes challenging situations, always
-        maintaining his positive attitude and working hard to meet all agreed
-        deadlines. Always proactive, adapts easily to changes and has a great
-        capacity for teamwork. If I had the chance to choose, I will always
-        choose him and his team to work on all projects. ”
-      </>
-    )
-  },
-  {
-    id: 20,
-    author_img: author_img_2,
-    brand_icon: brand_icon_2,
-    name: 'Helen Stewart',
-    title: 'Director',
-    description: (
-      <>
-        We have had a wonderful experience working with Dario and his team. They
-        communicate well and are eager and highly pleasant to work with. The
-        work is thorough and they are attentive to details and the particulars
-        of the requirements. We look forward to working with them again on
-        future projects. Cannot recommend them enough.
-      </>
-    )
-  },
-  {
-    id: 21,
-    author_img: eugene,
-    brand_icon: islandpay,
-    name: 'Eugene Luzgin',
-    title: 'CTO ',
-    description: (
-      <>
-        “ Highly recommend Gurudevelopers - they has great skills in IOS
-        development area. Will gladly work again when such need arises.”
-      </>
-    )
-  },
-  {
-    id: 22,
-    author_img: andres,
-    brand_icon: gds,
-    name: 'Andres Hernandez',
-    title: 'CEO ',
-    description: (
-      <>
-        “As the Director of GDS, I have had the privilege of working on several
-        projects . Their performance has always been extraordinary, not only in
-        technical aspects but also in his interpersonal skills with peers,
-        superiors, and subordinates. Additionally, their ability to lead and
-        motivate stood out. They are reliable, honest, proactive, and possesses
-        special abilities to thrive successfully in multidisciplinary work
-        teams.”
-      </>
-    )
-  },
-  {
-    id: 23,
-    author_img: veronica,
-    brand_icon: infinite,
-    name: 'Veronica Quiñonez',
-    title: 'Office Manager',
-    description: (
-      <>
-        “A pleasure to work with Agustín and Darío, good communication and on
-        time delivery. Thanks guys!”
-      </>
-    )
+    description:
+      '“A pleasure to work with Agustín and Darío, good communication and on time delivery. Thanks guys!”'
   }
 ]
 export default testimonial_data
