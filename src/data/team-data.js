f936c994afb1fbc71bbdb59eab0d3260dc7abee3
Agustin Sobral <asobralr@gmail.com>
2024-01-23 11:21:28 -0300

    Profile Agus

--- src/data/team-data.js
+++ src/data/team-data.js
@@ -14,27 +14,22 @@ const team_data = [
     cls: 1,
     img: team_img_1,
     name: "Agustín Sobral",
-    job_title: "Mobile Developer & Founder",
+    job_title: "Founder & Mobile Developer",
     slug: "agustin-sobral",
     feature_list: [
-      <>My set of skills includes in-app purchases, database interaction, push and local notifications, UI/UX development, Redux, Firebase, API integration, media streaming, user management, in-app advertisement, publishing on AppStore and PlayStore, etc</>,
+      <>
+        My set of skills includes database interaction, push and local notifications, UI/UX development, in-app purchases, Redux, Firebase, API integration, media streaming, user management, in-app advertisement, publishing on AppStore and PlayStore,
+        etc.
+      </>,
     ],
     expricence: [
-      { id: 1, title: "Role:", info: "Mobile senior React natve" },
+      { id: 1, title: "Role:", info: "Founder | Mobile Developer" },
       { id: 2, title: "Experience:", info: "16 years" },
     ],
     skill_title: "Superpower Skills:",
-    details_skill: ["Technical leader", "Positive Mindset", "Senior React Native"],
-   about_info_1: (
-      <>
-       Gurudevelopers is a way to live my life learning new techincal features and work with my friends.I'm an experienced app developer, using the React Native framework. 
-      </>
-    ),
-    about_info_2: (
-      <>
-      In the past, I've created an small beer company. I have 2 childs and I love to play soccer. 
-      </>
-    ),
+    details_skill: ["Technical leader", "React Native", "Analytical thinking"],
+    about_info_1: <>Gurudevelopers is a way to live my life doing what I love and working with my friends. I'm an experienced app developer, using the React Native framework.</>,
+    about_info_2: <>In the past, I've founded a small craft beer bewing company. I have 2 childs and I love to play soccer.</>,
     address: "Cañuelas, Buenos Aires, Argentina",
   },
   {
@@ -42,10 +37,13 @@ const team_data = [
     cls: 2,
     img: team_img_2,
     name: "Darío Mira",
-    job_title: "Mobile Developer & Founder",
+    job_title: "Founder & Mobile Developer",
     slug: "dario-mira",
     feature_list: [
-      <>From the first contact to the delivery of the requested solution, our work focuses on identifying and understanding the client’s needs. In this way, we seek to accompany them in the implementation of a technological development that best suits those needs.</>,
+      <>
+        From the first contact to the delivery of the requested solution, our work focuses on identifying and understanding the client’s needs. In this way, we seek to accompany them in the implementation of a technological development that best
+        suits those needs.
+      </>,
     ],
     expricence: [
       { id: 1, title: "Role:", info: " Mobile Developer Founder" },
@@ -53,16 +51,8 @@ const team_data = [
     ],
     skill_title: "Superpower Skills:",
     details_skill: ["Team motivation", "React Native", "Creativity"],
-    about_info_1: (
-      <>
-       Gurudevelopers is amazing to me. We are friends working together with selected customer
-      </>
-    ),
-    about_info_2: (
-      <>
-        I am art of living teacher. I guide online meditations weekly. I love to dance.
-      </>
-    ),
+    about_info_1: <>Gurudevelopers is amazing to me. We are friends working together with selected customer</>,
+    about_info_2: <>I am art of living teacher. I guide online meditations weekly. I love to dance.</>,
     address: "Palermo,  Buenos Aires, Argentina",
   },
 
@@ -81,10 +71,7 @@ const team_data = [
     skill_title: "Superpower Skills:",
     details_skill: ["React Js", "Next Js", "Mongo DB", "Node Js", "Express", "Typescript"],
     about_info_1: <>i love what I do. I complete the projects come what may</>,
-    feature_list: [
-      <>Back-end & Frond-end developer. I have rich experience in wordpress, also I am good at Magento, Sanity. </>,
-     
-    ],
+    feature_list: [<>Back-end & Frond-end developer. I have rich experience in wordpress, also I am good at Magento, Sanity. </>],
     about_info_2: <>I love to interact with people. I'm very good voleyball player</>,
     address: "Buenos Aires, ARG",
   },
@@ -95,25 +82,15 @@ const team_data = [
     name: "Nadia Aguidak",
     job_title: "UX/UI Designer",
     slug: "nadia-aguidak",
-    feature_list: [
-      <>Custom Design.</>,
-    ],
+    feature_list: [<>Custom Design.</>],
     expricence: [
       { id: 1, title: "Role:", info: "Design" },
       { id: 2, title: "Experience:", info: "10 years" },
     ],
     skill_title: "Superpower Skills:",
     details_skill: ["Figma", "Wordpress", "Adobe XD", "Adobe Photoshop"],
-    about_info_1: (
-      <>
-        Custom Design for your business or personal website. I have rich experience in Figma, also I am good at Adobe XD, Adobe Photoshop. 
-      </>
-    ),
-    about_info_2: (
-      <>
-       Once an year I spend 10 days in silence. Also I like to practice hot yoga.
-      </>
-    ),
+    about_info_1: <>Custom Design for your business or personal website. I have rich experience in Figma, also I am good at Adobe XD, Adobe Photoshop.</>,
+    about_info_2: <>Once an year I spend 10 days in silence. Also I like to practice hot yoga.</>,
   },
   {
     id: 5,
@@ -122,25 +99,15 @@ const team_data = [
     name: "Harald Solaas",
     job_title: "Fullstack Web Developer",
     slug: "harald-solaas",
-    feature_list: [
-      <>Gurudevelopers was my first Job. I'm very grateful with them</>,
-    ],
+    feature_list: [<>Gurudevelopers was my first Job. I'm very grateful with them</>],
     expricence: [
       { id: 1, title: "Role:", info: "Senior React Developer" },
       { id: 2, title: "Experience:", info: "8 years" },
     ],
     skill_title: "Superpower Skills:",
     details_skill: ["React", "Team leader", "Nextjs"],
-    about_info_1: (
-      <>
-        Constantly learning new technologies and frameworks. I have rich experience in React, also I am good at Nextjs, Nodejs, Wordpress, etc.
-      </>
-    ),
-    about_info_2: (
-      <>
-         I am an art of living teacher. I meditate twice a day. I play basketball
-      </>
-    ),
+    about_info_1: <>Constantly learning new technologies and frameworks. I have rich experience in React, also I am good at Nextjs, Nodejs, Wordpress, etc.</>,
+    about_info_2: <>I am an art of living teacher. I meditate twice a day. I play basketball</>,
   },
   {
     id: 6,
@@ -149,25 +116,15 @@ const team_data = [
     name: "Pedro Dorr",
     job_title: "Smart contract Developer",
     slug: "pedro-dorr",
-    feature_list: [
-      <>We made future Apps at Gurudevelopers. We are a great team</>,
-    ],
+    feature_list: [<>We made future Apps at Gurudevelopers. We are a great team</>],
     expricence: [
       { id: 1, title: "Role:", info: "Blockchain Developer" },
       { id: 2, title: "Experience:", info: "5 years" },
     ],
     skill_title: "Superpower Skills:",
     details_skill: ["Solidity", "Truffle", "Hardhat", "React", "Nextjs"],
-    about_info_1: (
-      <>
-        I have rich experience in Solidity, also I am good at Truffle, Hardhat, React, Nextjs, Nodejs, Wordpress, etc.
-      </>
-    ),
-    about_info_2: (
-      <>
-        Backend developer. I love Blockchain and Smart contracts because it is the future. I am a good team leader.
-      </>
-    ),
+    about_info_1: <>I have rich experience in Solidity, also I am good at Truffle, Hardhat, React, Nextjs, Nodejs, Wordpress, etc.</>,
+    about_info_2: <>Backend developer. I love Blockchain and Smart contracts because it is the future. I am a good team leader.</>,
   },
   {
     id: 7,
@@ -176,25 +133,15 @@ const team_data = [
     name: "Dario Hoyos",
     job_title: "Web Developer",
     slug: "dario-hoyos",
-    feature_list: [
-      <> I like to be responsible to take care of projects</>,
-    ],
+    feature_list: [<> I like to be responsible to take care of projects</>],
     expricence: [
       { id: 1, title: "Role:", info: "Design" },
       { id: 2, title: "Experience:", info: "10 years" },
     ],
     skill_title: "Superpower Skills:",
     details_skill: ["PHP", "Wordpress", "Laravel", "React"],
-    about_info_1: (
-      <>
-        I have rich experience in PHP, also I am good at Laravel, React, Wordpress, etc.
-      </>
-    ),
-    about_info_2: (
-      <>
-        I love to interact with people. I want to be influencer.
-      </>
-    ),
+    about_info_1: <>I have rich experience in PHP, also I am good at Laravel, React, Wordpress, etc.</>,
+    about_info_2: <>I love to interact with people. I want to be influencer.</>,
   },
   {
     id: 8,
@@ -203,9 +150,7 @@ const team_data = [
     name: "Maximiliano Albarran",
     job_title: "Junior Web Developer",
     slug: "maximiliano-albarran",
-    feature_list: [
-      <>I like to work in freedom and good companies</>,
-    ],
+    feature_list: [<>I like to work in freedom and good companies</>],
     expricence: [
       { id: 1, title: "Role:", info: "Frontend Web Developer" },
       { id: 2, title: "Experience:", info: "1 year" },
@@ -220,16 +165,8 @@ const team_data = [
     twitter: "",
     linkedin: "https://www.linkedin.com/in/maximiliano-albarr%C3%A1n/",
     address: "Buenos Aires, ARG",
-    about_info_1: (
-      <>
-        I have rich experience in React, also I am good at Nextjs, Nodejs, Wordpress, etc.
-      </>
-    ),
-    about_info_2: (
-      <>
-        I'm good to work in team. Also I am a good voleyball player.
-      </>
-    ),
+    about_info_1: <>I have rich experience in React, also I am good at Nextjs, Nodejs, Wordpress, etc.</>,
+    about_info_2: <>I'm good to work in team. Also I am a good voleyball player.</>,
   },
 ];
 
