a9d4431910af9266f8650b4d756ccc980da5d59c
miradario <miradario@gmail.com>
2024-01-29 22:37:56 -0300

    recomendation

--- src/data/testimonial-data.js
+++ src/data/testimonial-data.js
@@ -2,8 +2,8 @@ import author_img_2 from '../../public/assets/img/testimonial/testi-icon-1.jpeg'
 import eugene from '../../public/assets/img/testimonial/eugene.jpeg'
 import veronica from '../../public/assets/img/testimonial/veronica.jpeg'
 import andres from '../../public/assets/img/testimonial/andres.jpeg'
-import author_img_5 from '../../public/assets/img/testimonial/testi-icon-5-4.png'
-import author_img_6 from '../../public/assets/img/testimonial/testi-icon-5-1.png'
+import valerie from '../../public/assets/img/testimonial/valerie.jpeg'
+import christoph from '../../public/assets/img/testimonial/christoph.jpeg'
 import author_img_8 from '../../public/assets/img/testimonial/testi-icon-5-2.png'
 import author_img_9 from '../../public/assets/img/testimonial/testi-icon-5-4.png'
 import author_img_12 from '../../public/assets/img/testimonial/santi.jpeg'
@@ -12,29 +12,23 @@ import azero from '../../public/assets/img/testimonial/a0.png'
 import islandpay from '../../public/assets/img/testimonial/islandpay.svg'
 import infinite from '../../public/assets/img/testimonial/infinitepower.png'
 import gds from '../../public/assets/img/testimonial/gdsc.png'
+import brainbo from '../../public/assets/img/testimonial/brainbp.png'
+
 import brand_icon_6 from '../../public/assets/img/testimonial/testi-logo-5-2.png'
 import brand_icon_8 from '../../public/assets/img/testimonial/testi-logo-5-1.png'
 import brand_icon_9 from '../../public/assets/img/testimonial/testi-logo-5-2.png'
 import brand_icon_12 from '../../public/assets/img/testimonial/tlexlogo.png'
 
 const testimonial_data = [
+
   {
-    id: 12,
-    author_img: author_img_12,
-    brand_icon: brand_icon_12,
-    name: 'Santiago Facal',
-    title: ' Product Manager',
+    id: 18,
+    author_img: valerie,
+    brand_icon: brainbo,
+    name: 'Valerie Corcias',
+    title: 'Owner',
     description:
-      '“ The results are even more than expected, he and his team are responsible and fully committed to every project we work on. They responds to changing and sometimes challenging situations, always maintaining his positive attitude and working hard to meet all agreed deadlines. Always proactive, adapts easily to changes and has a great capacity for teamwork. If I had the chance to choose, I will always choose him and his team to work on all projects. ”'
-  },
-  {
-    id: 13,
-    author_img: author_img_2,
-    brand_icon: azero,
-    name: 'Helen Stewart',
-    title: 'Director',
-    description:
-      'We have had a wonderful experience working with Dario and his team. They communicate well and are eager and highly pleasant to work with. The work is thorough and they are attentive to details and the particulars of the requirements. We look forward to working with them again on future projects. Cannot recommend them enough.'
+      '“Working with Guru Developers team has been an absolute pleasure. They are truly one-of-a-kind creative developers who not only understand but also synergize perfectly with our apps concept. Their responsiveness and readiness to innovate in this field have been impressive. We highly recommend them for their exceptional skills and collaborative approach”'
   },
   {
     id: 14,
@@ -45,6 +39,34 @@ const testimonial_data = [
     description:
       '“ Highly recommend Gurudevelopers - they has great skills in IOS development area. Will gladly work again when such need arises.”'
   },
+    {
+    id: 13,
+    author_img: christoph,
+    brand_icon: brand_icon_12,
+    name: 'Christoph',
+    title: 'Tlex Team',
+    description:
+      'Thank you so much for all the hard work, for all the creative ideas, the great push to make the Tlex App happen! We have been dreaming for an App for years and thanks to Gurudevelopers it has happened now'
+  },
+  {
+    id: 13,
+    author_img: author_img_2,
+    brand_icon: azero,
+    name: 'Helen Stewart',
+    title: 'Director',
+    description:
+      'We have had a wonderful experience working with Dario and his team. They communicate well and are eager and highly pleasant to work with. The work is thorough and they are attentive to details and the particulars of the requirements. We look forward to working with them again on future projects. Cannot recommend them enough.'
+  },
+
+  {
+    id: 12,
+    author_img: author_img_12,
+    brand_icon: brand_icon_12,
+    name: 'Santiago Facal',
+    title: ' Product Manager',
+    description:
+      '“ The results are even more than expected, he and his team are responsible and fully committed to every project we work on. They responds to changing and sometimes challenging situations, always maintaining his positive attitude and working hard to meet all agreed deadlines. Always proactive, adapts easily to changes and has a great capacity for teamwork. If I had the chance to choose, I will always choose him and his team to work on all projects. ”'
+  },
   {
     id: 17,
     author_img: andres,
@@ -54,6 +76,7 @@ const testimonial_data = [
     description:
       '“As the Director of GDS, I have had the privilege of working on several projects . Their performance has always been extraordinary, not only in technical aspects but also in his interpersonal skills with peers, superiors, and subordinates. Additionally, their ability to lead and motivate stood out. They are reliable, honest, proactive, and possesses special abilities to thrive successfully in multidisciplinary work teams.”'
   },
+  
   {
     id: 18,
     author_img: veronica,
@@ -62,6 +85,9 @@ const testimonial_data = [
     title: 'Office Manager',
     description:
       '“A pleasure to work with Agustín and Darío, good communication and on time delivery. Thanks guys!”'
-  }
+  },
+
+
+  
 ]
 export default testimonial_data
